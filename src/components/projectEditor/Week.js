75b9774dd5a99c8b810c0727862f1c7c67457682
Josiah <jo.launchpad@gmail.com>
2022-03-17 20:52:10 -0400

    Fix auto scroll; Make active week auto scroll farther from top

--- src/components/projectEditor/Week.js
+++ src/components/projectEditor/Week.js
@@ -17,18 +17,18 @@ export default function Week({ id, container }) {
   const startDate = new Date(useSelector((state) => state.projects[projectId].startDate));
   const weekIds = Object.keys(useSelector((state) => state.projects[projectId].weeks));
   const dayOffset = (weekIds.length - weekIds.indexOf(id) - 1) * 7 - startDate.getDay();
-  const weekStartDate = new Date((startDate.getTime() + dayOffset * 86400000));
-  const weekEndDate = new Date((weekStartDate.getTime() + 518400000));
+  const weekStartDate = new Date(startDate.getTime() + dayOffset * 86400000);
+  const weekEndDate = new Date(weekStartDate.getTime() + 518400000);
 
   const [selectedDay, setSelectedDay] = useState(0);
   const weekContainer = useRef(null);
   let weekPos = useRef(null);
 
+	// Calculate position in parent container
   useEffect(() => {
     weekPos.current =
-      weekContainer.current.getBoundingClientRect().top -
-      (selectedWeek ? 100 : 340) -
-      container.current.getBoundingClientRect().top;
+      weekContainer.current.getBoundingClientRect().top - container.current.getBoundingClientRect().top - 200;
+    console.log(weekPos.current);
   }, []);
 
   function selectThisWeek() {
@@ -42,8 +42,8 @@ export default function Week({ id, container }) {
     <div onClick={selectThisWeek} ref={weekContainer} className={styles.container}>
       <p className={styles.startDate}>{weekStartDate.toString().slice(4, 10)}</p>
       <p className={styles.endDate}>{weekEndDate.toString().slice(4, 10)}</p>
-      <BarGraph mode={selectedWeek ? "active" : "inactive"} weekId={id} setSelectedDay={setSelectedDay} />
-      {selectedWeek && <DaySplits weekId={id} selectedDay={selectedDay} weekListRef={container} />}
+      <BarGraph mode={selectedWeek && weekPos.current ? "active" : "inactive"} weekId={id} setSelectedDay={setSelectedDay} />
+      {selectedWeek && weekPos.current && <DaySplits weekId={id} selectedDay={selectedDay} weekListRef={container} />}
     </div>
   );
 }
