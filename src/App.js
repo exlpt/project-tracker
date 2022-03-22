37e705e9f39f7f3da5852252f00e03844e074177
Josiah <jo.launchpad@gmail.com>
2022-03-22 16:46:06 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage.js";
 import ProjectsPage from "./pages/ProjectsPage.js";
 import ProjectEditorPage from "./pages/ProjectEditorPage.js";
 
 export default function App() {
-	return (
-		<div>
-			<ProjectEditorPage />
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProjectsPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="editor" element={<ProjectEditorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
